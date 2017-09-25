05a90f81dbc6236d78b1f12e489af692a0b77f23
gitbot09 <25991646+gitbot09@users.noreply.github.com>
2017-09-25 18:06:05 -0500

    Update cleanup.sh

--- inflation_resources/scripts/maintenance_scripts/cleanup.sh
+++ inflation_resources/scripts/maintenance_scripts/cleanup.sh
@@ -45,6 +45,26 @@ echo "clearing ubuntu14-8192mb box"
 cd ~/.inflation/vbox_machine_repos/ubuntu14-8192mb
 vagrant destroy -f --no-color
 
+echo "clearing ubuntu16-512mb box"
+cd ~/.inflation/vbox_machine_repos/ubuntu16-512mb
+vagrant destroy -f --no-color
+
+echo "clearing ubuntu16-1024mb box"
+cd ~/.inflation/vbox_machine_repos/ubuntu16-1024mb
+vagrant destroy -f --no-color
+
+echo "clearing ubuntu16-2048mb box"
+cd ~/.inflation/vbox_machine_repos/ubuntu16-2048mb
+vagrant destroy -f --no-color
+
+echo "clearing ubuntu16-4096mb box"
+cd ~/.inflation/vbox_machine_repos/ubuntu16-4096mb
+vagrant destroy -f --no-color
+
+echo "clearing ubuntu16-8192mb box"
+cd ~/.inflation/vbox_machine_repos/ubuntu16-8192mb
+vagrant destroy -f --no-color
+
 echo "clearing vagrant boxes"
 vagrant box list | grep 'There are no installed boxes! Use `vagrant box add` to add some.' &> /dev/null
 if ! [ $? == 0 ]; then
