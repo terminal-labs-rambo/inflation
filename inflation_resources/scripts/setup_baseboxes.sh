ed8f5f5ecc3580c18f293703fbbaae0d2d1029d6
user <michael@terminallabs.com>
2017-10-05 13:11:34 -0500

    cleanup.sh and setup_baseboxes.sh now use yasha/jinja

--- inflation_resources/scripts/setup_baseboxes.sh
+++ inflation_resources/scripts/setup_baseboxes.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 mkdir -p inflation_resources/compiled_scripts
 
 yasha inflation_resources/yasha_templates/bash_scripts/setup_baseboxes.sh.template -o inflation_resources/compiled_scripts/setup_baseboxes.sh
