e62e577fa703305f3039d3821cf6f0f14c434b5a
Michael Verhulst <michael@terminallabs.com>
2017-11-24 23:25:55 -0600

    Update saltmaster.sh

--- inflation_resources/bash_aliases/saltmaster.sh
+++ inflation_resources/bash_aliases/saltmaster.sh
@@ -6,11 +6,3 @@ eval "$(ssh-agent -s)"
 ssh-add /home/saltmaster/salt_controlplane/keys/master
 
 alias inflation="python internal_cli.py"
-
-function inflation-salt-call { # $1 = location, e.g. 'master' or '*', $2 = command
-    start="su saltmaster -c \"cd /home/saltmaster; source salt_venv/bin/activate; python /home/saltmaster/salt_src/scripts/salt '"
-    middle="' -c /home/saltmaster/salt_controlplane/etc/salt "
-    end=" \""
-    command=$start$1$middle$2$end
-    eval $command
-}
