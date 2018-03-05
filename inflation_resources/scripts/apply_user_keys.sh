57f1e100a19667b00e864b315160d4143412ce5a
Michael Verhulst <michael@terminallabs.com>
2018-03-05 17:54:21 -0600

    now we can run a apply_user_keys salt state prototype on all nodes

--- inflation_resources/scripts/apply_user_keys.sh
+++ inflation_resources/scripts/apply_user_keys.sh
@@ -5,3 +5,4 @@ vagrant ssh -c "sudo -H -u root bash -c 'cp /home/vagrant/.ssh/id_rsa /home/salt
 vagrant ssh -c "sudo -H -u root bash -c 'cp /home/vagrant/.ssh/id_rsa.pub /home/saltmaster/salt_controlplane/etc/salt/keys/id_rsa.pub'"
 vagrant ssh -c "sudo -H -u root bash -c 'chown saltmaster /home/saltmaster/salt_controlplane/etc/salt/keys -R'"
 vagrant ssh -c "sudo -H -u root bash -c 'chgrp saltmaster /home/saltmaster/salt_controlplane/etc/salt/keys -R'"
+vagrant ssh -c "sudo su saltmaster -c 'cd /home/saltmaster; source salt_venv/bin/activate; python /home/saltmaster/salt_src/scripts/salt '\'*\'' state.sls cluster_init.apply_user_keys -c /home/saltmaster/salt_controlplane/etc/salt --timeout 1 --no-color'"
