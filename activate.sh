38cd3fdd76107bfb4d836b6fec6bd028f53b44c7
Michael Verhulst <michael@terminallabs.com>
2018-03-27 11:25:20 -0500

    removed activate.sh

--- activate.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-alias root-init="sudo bash inflation_resources/scripts/install_scripts/linux_install_root.sh"
-alias init="bash inflation_resources/scripts/maintenance_scripts/init.sh"
-
-alias inflate="bash inflation_resources/scripts/inflate.sh"
-alias deflate="bash inflation_resources/scripts/deflate.sh"
-alias cleanup="bash inflation_resources/scripts/maintenance_scripts/cleanup.sh"
-alias apply_user_keys="bash inflation_resources/scripts/apply_user_keys.sh"
