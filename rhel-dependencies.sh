b988d1a6470aa59c5ccf5cd624dfa61ccfb27fa2
Michael Verhulst <michael@terminallabs.com>
2019-03-25 21:45:07 -0500

    Update rhel-dependencies.sh

--- rhel-dependencies.sh
+++ rhel-dependencies.sh
@@ -8,10 +8,3 @@ yum -y groupinstall 'Development Tools'
 yum -y install wget
 yum -y install git
 yum -y install mercurial
-
-wget https://www.virtualbox.org/download/oracle_vbox.asc
-rpm --import oracle_vbox.asc
-wget http://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo -O /etc/yum.repos.d/virtualbox.repo
-yum install -y "VirtualBox-5.2"
-/usr/lib/virtualbox/vboxdrv.sh setup
-rm oracle_vbox.asc*
