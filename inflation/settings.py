5bbc6c7e2e38d1dfb262132d7c390f875f395d5e
verhulstm <michael@terminallabs.com>
2019-03-26 00:22:36 -0500

    removed python inflation client

--- inflation/settings.py
+++
@@ -1,8 +0,0 @@
-import os
-
-VERSION='0.0.5.dev'
-PROJECT_NAME = 'inflation'
-HOME = os.path.expanduser('~')
-SALT_MASTER_RAMBO_PROJECT_NAME = '.inflation-master'
-SALT_MASTER_RAMBO_PROJECT_LOCATION = os.path.abspath(os.path.join(os.path.realpath(os.getcwd()), SALT_MASTER_RAMBO_PROJECT_NAME))
-CLUSTER_METADATA_DIR = SALT_MASTER_RAMBO_PROJECT_LOCATION + '/.inflation-tmp'
