4e12516d63c4e095c6d85ebdcc39f475b2431c20
verhulstm <michael@terminallabs.com>
2019-03-25 23:27:22 -0500

    addedin a new rambo master box

--- inflation_resources/scripts/vagrant_commands.sh
+++ inflation_resources/scripts/vagrant_commands.sh
@@ -2,9 +2,11 @@
 
 vendor=$(<.tmp/vendor)
 
+cd master
 echo "starting salt master node"
 rambo up -o ubuntu-1604
 echo "master node is up"
+cd ..
 
 echo "initilising cluster"
 vagrant  ssh -c "sudo bash /vagrant/inflation_resources/scripts/salt_commands.sh" 2> /dev/null
