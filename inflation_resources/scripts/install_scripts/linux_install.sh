fd6762e88e7b895667e28ee0f155902e00dc2c8e
Michael Verhulst <michael@terminallabs.com>
2019-03-25 22:16:47 -0500

    Update linux_install.sh

--- inflation_resources/scripts/install_scripts/linux_install.sh
+++ inflation_resources/scripts/install_scripts/linux_install.sh
@@ -3,10 +3,3 @@
 mkdir -p ~/.inflation
 mkdir -p ~/.inflation/bin
 mkdir -p ~/.inflation/build
-
-cd ~/.inflation/build
-git clone https://github.com/lastpass/lastpass-cli.git
-cd lastpass-cli
-cmake . && make
-
-cp ~/.inflation/build/lastpass-cli/lpass ~/.inflation/bin/lpass
