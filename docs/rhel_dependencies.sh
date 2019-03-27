fd4c3e9252c7e26f9dcd046b53dd1c9aa1b85fa4
Michael Verhulst <michael@terminallabs.com>
2019-03-27 01:36:06 -0500

    Update rhel_dependencies.sh

--- docs/rhel_dependencies.sh
+++ docs/rhel_dependencies.sh
@@ -1,8 +1,8 @@
-yum -y check-update
-yum -y upgrade
-
 yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E '%{rhel}').noarch.rpm
 yum -y install https://$(rpm -E '%{?centos:centos}%{!?centos:rhel}%{rhel}').iuscommunity.org/ius-release.rpm
+
+yum -y check-update
+yum -y upgrade
 
 yum -y groupinstall 'Development Tools'
 yum -y install wget
