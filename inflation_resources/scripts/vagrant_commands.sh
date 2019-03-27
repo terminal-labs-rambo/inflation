3fa1dc7c1c279258444063454b4f188591c40970
verhulstm <michael@terminallabs.com>
2019-03-27 00:41:11 -0500

    now copying over auth and .tmp dirs

--- inflation_resources/scripts/vagrant_commands.sh
+++ inflation_resources/scripts/vagrant_commands.sh
@@ -6,6 +6,8 @@ echo "starting salt master node"
 rambo up -o ubuntu-1604
 rambo scp inflation_resources /vagrant/inflation_resources
 rambo scp scripts /vagrant/scripts
+rambo scp auth/keys /vagrant/auth/keys
+rambo scp .tmp /vagrant/.tmp
 rambo ssh -c "bash\ /vagrant/inflation_resources/scripts/setup_master.sh"
 echo "master node is up"
 
