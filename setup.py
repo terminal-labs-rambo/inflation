4f88d9d323c8691db77aa44e635ac7f374cfc949
Michael Verhulst <michael@terminallabs.com>
2019-08-31 19:21:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,13 +30,6 @@ setup(
         'termcolor',
         'pyyaml',
     ],
-    classifiers = [
-            "Programming Language :: Python :: 3.2",
-            "Programming Language :: Python :: 3.3",
-            "Programming Language :: Python :: 3.4",
-            "Programming Language :: Python :: 3.5",
-            "Programming Language :: Python :: 3.6",
-    ],
     entry_points='''
         [console_scripts]
         inflation=inflation.cli:main
