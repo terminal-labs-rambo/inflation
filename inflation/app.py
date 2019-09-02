55105864d053401c942ca2a85949f0942412e0de
verhulstm <michael@terminallabs.com>
2019-09-02 19:57:17 +0000

    now inflation grabs inflation_resources and injects them into the master rambo project

--- inflation/app.py
+++ inflation/app.py
@@ -30,6 +30,10 @@ def init():
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+    directory = HOME + "/.inflation/tmp"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
     target = os.path.abspath(HOME + "/.inflation/vagrantfiles")
     if not os.path.exists(target):  # Do not overwrite existing saltstack dir. Installs don't delete!
         url = "https://github.com/terminal-labs/vagrantfiles/archive/master.zip"
@@ -65,6 +69,24 @@ def init():
             zf.extractall()
         shutil.move(os.path.abspath("rambo_inflation-master-master"), target)
         os.remove(filename)
+
+    target = os.path.abspath(HOME + "/.inflation/tmp/inflation")
+    if not os.path.exists(target):  # Do not overwrite existing dir. Installs don't delete!
+        url = "https://github.com/terminal-labs/inflation/archive/master.zip"
+        filename = ".inflation/tmp/inflation.zip"
+        with urllib.request.urlopen(url) as response, open(filename, "wb") as out_file:
+            shutil.copyfileobj(response, out_file)
+        zipfile = filename
+        with ZipFile(zipfile) as zf:
+            zf.extractall(".inflation/tmp/inflation")
+        if os.path.exists(".inflation/tmp/inflation_resources"):
+            shutil.rmtree(".inflation/tmp/inflation_resources", ignore_errors=True)
+        shutil.move(".inflation/tmp/inflation/inflation-master/inflation_resources", ".inflation/tmp/inflation_resources")
+        if os.path.exists("inflation-master/inflation_resources"):
+            shutil.rmtree("inflation-master/inflation_resources", ignore_errors=True)
+        shutil.move(".inflation/tmp/inflation_resources", "inflation-master")
+        os.remove(filename)
+        shutil.rmtree(".inflation/tmp/inflation", ignore_errors=True)
 
 
 def inflate(filepath):
