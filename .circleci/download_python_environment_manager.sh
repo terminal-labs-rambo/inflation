e929f0225742b8ae3b1ba3a29aaf21e3c83880ea
Michael Verhulst <michael@terminallabs.com>
2020-02-02 14:13:22 -0600

    Update download_python_environment_manager.sh

--- .circleci/download_python_environment_manager.sh
+++ .circleci/download_python_environment_manager.sh
@@ -3,9 +3,5 @@ su -m vagrant <<'EOF'
   mkdir -p .tmp
   cd .tmp
   wget https://github.com/terminal-labs/python-environment-manager/archive/master.zip
-  unzip -qq master.zip
-  cp -r python-environment-manager-master/maintenance/. ../maintenance
-  rm -rf python-environment-manager-master
-  rm master.zip
-  cd ..
+  unzip master.zip
 EOF
