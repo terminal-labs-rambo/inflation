a56b13cd51cc25028eec4f0da0db77e5c240d82d
verhulstm <michael@terminallabs.com>
2020-05-06 15:03:10 -0500

    fix

--- inflation/inflation/app.py
+++ inflation/inflation/app.py
@@ -9,7 +9,8 @@ from zipfile import ZipFile
 from inflation.config_parser import process_spec_file
 from rambo.app import up, destroy, ssh, set_init_vars
 
-HOME = "/vagrant"
+#HOME = "/vagrant"
+HOME = "."
 PROJECT_LOCATION = os.path.dirname(os.path.realpath(__file__))
 SALT_MASTER_RAMBO_PROJECT_NAME = os.path.join(PROJECT_LOCATION, "..", "inflation-master")
 
@@ -87,75 +88,42 @@ def loadkeys():
         else:
             print("cant find keys dir ------------- success")
 
+def _create_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def _get(url, target, filename, zipname):
+    if not os.path.exists(target):
+        with urllib.request.urlopen(url) as response, open(filename, "wb") as out_file:
+            shutil.copyfileobj(response, out_file)
+        zipfile = filename
+        with ZipFile(zipfile) as zf:
+            zf.extractall()
+        shutil.move(os.path.abspath(zipname), target)
+        os.remove(filename)
 
 def init():
-    directory = os.path.join(HOME, ".inflation")
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    _create_dir(os.path.join(HOME, ".inflation"))
+    _create_dir(os.path.join(HOME, ".inflation", "minion_repos"))
+    _create_dir(os.path.join(HOME, ".inflation", "build"))
+    _create_dir(os.path.join(HOME, ".inflation", "bin"))
+    _create_dir(os.path.join(HOME, ".inflation", "tmp"))
 
-    directory = os.path.join(HOME, ".inflation", "minion_repos")
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    _get("https://github.com/terminal-labs/vagrantfiles/archive/master.zip",
+        os.path.abspath(os.path.join(HOME, ".inflation", "vagrantfiles")),
+        "vagrantfiles.zip",
+        "vagrantfiles-master")
 
-    directory = os.path.join(HOME, ".inflation", "build")
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    _get("https://github.com/terminal-labs/simple-vbox-server/archive/master.zip",
+        os.path.abspath(os.path.join(HOME, ".inflation", "simple-vbox-server")),
+        "simple-vbox-server.zip",
+        "simple-vbox-server-master")
 
-    directory = os.path.join(HOME, ".inflation", "bin")
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    _get("https://github.com/terminal-labs/rambo_inflation-clustermaster/archive/master.zip",
+        os.path.abspath(os.path.join(HOME, "inflation-master")),
+        "rambo_inflation-clustermaster.zip",
+        "rambo_inflation-clustermaster-master")
 
-    directory = os.path.join(HOME, ".inflation", "tmp")
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    target = os.path.abspath(os.path.join(HOME, ".inflation", "vagrantfiles"))
-    if not os.path.exists(target):  # Do not overwrite existing saltstack dir. Installs don't delete!
-        url = "https://github.com/terminal-labs/vagrantfiles/archive/master.zip"
-        filename = "vagrantfiles.zip"
-        with urllib.request.urlopen(url) as response, open(filename, "wb") as out_file:
-            shutil.copyfileobj(response, out_file)
-        zipfile = filename
-        with ZipFile(zipfile) as zf:
-            zf.extractall()
-        shutil.move(os.path.abspath("vagrantfiles-master"), target)
-        os.remove(filename)
-
-    target = os.path.abspath(os.path.join(HOME, ".inflation", "simple-vbox-server"))
-    if not os.path.exists(target):  # Do not overwrite existing saltstack dir. Installs don't delete!
-        url = "https://github.com/terminal-labs/simple-vbox-server/archive/master.zip"
-        filename = "simple-vbox-server.zip"
-        with urllib.request.urlopen(url) as response, open(filename, "wb") as out_file:
-            shutil.copyfileobj(response, out_file)
-        zipfile = filename
-        with ZipFile(zipfile) as zf:
-            zf.extractall()
-        shutil.move(os.path.abspath("simple-vbox-server-master"), target)
-        os.remove(filename)
-
-    target = os.path.abspath(os.path.join(HOME, "inflation-master"))
-    if not os.path.exists(target):  # Do not overwrite existing dir. Installs don't delete!
-        url = "https://github.com/terminal-labs/rambo_inflation-master/archive/master.zip"
-        filename = "rambo_inflation-master.zip"
-        with urllib.request.urlopen(url) as response, open(filename, "wb") as out_file:
-            shutil.copyfileobj(response, out_file)
-        zipfile = filename
-        with ZipFile(zipfile) as zf:
-            zf.extractall()
-        shutil.move(os.path.abspath("rambo_inflation-master-master"), target)
-        os.remove(filename)
-
-    target = os.path.abspath(os.path.join(HOME, ".inflation", "tmp", "inflation"))
-
-    url = "https://github.com/terminal-labs/inflation/archive/master.zip"
-    filename = os.path.abspath(os.path.join(HOME, ".inflation", "tmp", "inflation.zip"))
-    with urllib.request.urlopen(url) as response, open(filename, "wb") as out_file:
-        shutil.copyfileobj(response, out_file)
-    zipfile = filename
-    with ZipFile(zipfile) as zf:
-        zf.extractall(".inflation/tmp/inflation")
-    if os.path.exists(".inflation/tmp/inflation_resources"):
-         shutil.rmtree(".inflation/tmp/inflation_resources", ignore_errors=True)
     # shutil.move(".inflation/tmp/inflation/inflation-master/inflation_resources", ".inflation/tmp/inflation_resources")
     # if os.path.exists("inflation-master/inflation_resources"):
     #     shutil.rmtree("inflation-master/inflation_resources", ignore_errors=True)
