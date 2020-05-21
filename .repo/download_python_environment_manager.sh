8ff65e118acc31d20fb901b03f28f44abccdf110
Michael Verhulst <michael@terminallabs.com>
2020-05-21 00:42:35 -0500

    Update download_python_environment_manager.sh

--- .repo/download_python_environment_manager.sh
+++ .repo/download_python_environment_manager.sh
@@ -2,6 +2,6 @@ su -m vagrant <<'EOF'
   cd /vagrant
   mkdir -p .tmp
   cd .tmp
-  wget https://github.com/terminal-labs/python-environment-manager/archive/master.zip
+  wget https://github.com/terminal-labs/bash-environment-manager/archive/master.zip
   unzip master.zip
 EOF
