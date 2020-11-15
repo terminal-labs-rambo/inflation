ccbae06d40be2519c82884756d8e53e39b9b27ac
Michael Verhulst <michael@terminallabs.com>
2020-11-15 00:35:34 -0600

    Update derived_settings.py

--- inflation/derived_settings.py
+++ inflation/derived_settings.py
@@ -2,24 +2,11 @@ import os
 import site
 import tempfile
 
+APPDIR = os.path.abspath(os.path.dirname(__file__))
+SETUPFILEDIR = os.path.abspath(os.path.join(APPDIR, ".."))
+TESTDIR = os.path.abspath(os.path.join(APPDIR, "tests"))
+MEMTEMPDIR = "/dev/shm"
+SITEPACKAGESPATH = site.getsitepackages()[0]
 
-def set_vars():
-    APPDIR = os.path.abspath(os.path.dirname(__file__))
-    SETUPFILEDIR = os.path.abspath(os.path.join(APPDIR, ".."))
-    TESTDIR = os.path.abspath(os.path.join(APPDIR, "tests"))
-    MEMTEMPDIR = "/dev/shm"
-    SITEPACKAGESPATH = site.getsitepackages()[0]
-
-    if os.path.isdir(MEMTEMPDIR):
-        tempfile.tempdir = MEMTEMPDIR
-
-    globals()["APPDIR"] = APPDIR
-    globals()["SETUPFILEDIR"] = SETUPFILEDIR
-    globals()["TESTDIR"] = TESTDIR
-    globals()["MEMTEMPDIR"] = MEMTEMPDIR
-    globals()["SITEPACKAGESPATH"] = SITEPACKAGESPATH
-
-    return {"APPDIR": APPDIR, "SETUPFILEDIR": SETUPFILEDIR, "TESTDIR": TESTDIR, "MEMTEMPDIR": MEMTEMPDIR, "SITEPACKAGESPATH": SITEPACKAGESPATH}
-
-
-set_vars()
+if os.path.isdir(MEMTEMPDIR):
+    tempfile.tempdir = MEMTEMPDIR
