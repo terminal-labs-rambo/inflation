f5bcd74b0f08f7ecaf351bb941816c6de4cffc22
verhulstm <michael@terminallabs.com>
2020-11-21 17:56:54 -0600

    code cleaning

--- inflation/app.py
+++ inflation/app.py
@@ -1,4 +1,5 @@
 import os
+
 from os.path import dirname, realpath, abspath, join, exists
 from configparser import ConfigParser
 
@@ -9,8 +10,10 @@ from inflation.settings import *
 
 HOME = "."
 PROJECT_LOCATION = dirname(realpath(__file__))
-inflation_master_path = abspath(join(HOME, ".tmp", "artifacts", "nucleation"))
-inflation_master_tmp = abspath(join(inflation_master_path, ".tmp"))
+primary_nucleation_path = abspath(join(HOME, ".tmp", "artifacts", "primary-nucleation"))
+primary_nucleation_tmp = abspath(join(primary_nucleation_path, ".tmp"))
+secondary_nucleation_path = abspath(join(primary_nucleation_tmp, "artifacts", "secondary-nucleation"))
+secondary_nucleation_tmp = abspath(join(secondary_nucleation_path, ".tmp"))
 # URLS = {
 #     "GITHUBBASE": "https://github.com/terminal-labs"
 # }
@@ -52,6 +55,27 @@ def resync():
     pass
 
 
+def _prep_primary_nucleation():
+    _copy_dir(abspath(join(HOME, ".tmp", "primary-nucleation-master")), primary_nucleation_path)
+
+    _create_dir(primary_nucleation_tmp)
+    _copy_dir(abspath(join(HOME, "auth")), abspath(join(primary_nucleation_tmp, "auth")))
+
+    _get_github_repo(
+        "https://github.com/terminal-labs/secondary-nucleation",
+        abspath(join(primary_nucleation_tmp, "primary-nucleation.zip")),
+        abspath(join(primary_nucleation_tmp, "primary-nucleation.zip")),
+        primary_nucleation_tmp
+    )
+
+
+def _prep_secondary_nucleation():
+    _copy_dir(abspath(join(primary_nucleation_tmp, "secondary-nucleation-master")), secondary_nucleation_path)
+
+    _create_dir(secondary_nucleation_tmp)
+    _copy_dir(abspath(join(HOME, "auth")), abspath(join(secondary_nucleation_tmp, "auth")))
+
+
 def inflate(filepath):
     # print(loadkeysdict())
     # process_spec_file(filepath)
@@ -70,31 +94,32 @@ def inflate(filepath):
         abspath(join(HOME, ".tmp"))
     )
     _get_github_repo(
-        "https://github.com/terminal-labs/nucleation",
-        abspath(join(HOME, ".tmp", "nucleation.zip")),
-        abspath(join(HOME, ".tmp", "nucleation.zip")),
+        "https://github.com/terminal-labs/primary-nucleation",
+        abspath(join(HOME, ".tmp", "primary-nucleation.zip")),
+        abspath(join(HOME, ".tmp", "primary-nucleation.zip")),
         abspath(join(HOME, ".tmp"))
     )
 
-    _create_dir(".tmp/artifacts")
-    _copy_dir(abspath(join(HOME, ".tmp", "nucleation-master")), inflation_master_path)
-
-    _create_dir(inflation_master_tmp)
     _copy_dir(abspath(join(HOME, "auth")), abspath(join(HOME, ".tmp", "auth")))
-    _copy_dir(abspath(join(HOME, "auth")), abspath(join(inflation_master_tmp, "auth")))
 
-    os.chdir(inflation_master_path)
-    up(provider="virtualbox", tmpdir=inflation_master_tmp)
+    _create_dir(".tmp/artifacts")
+    _prep_primary_nucleation()
+
+    _create_dir(join(primary_nucleation_tmp, "artifacts"))
+    _prep_secondary_nucleation()
+
+    os.chdir(primary_nucleation_path)
+    #up(provider="virtualbox", tmpdir=primary_nucleation_tmp)
     #     # ssh(command="'sudo bash /vagrant/scripts/salt-cloud-commands-prepare-master.sh'")
     #     # ssh(command="'sudo bash /vagrant/scripts/salt-cloud-commands-spawn-minions.sh'")
     #     # ssh(command="'sudo bash /vagrant/scripts/salt-cloud-commands-prepare-cluster.sh'")
 
 
 def deflate():
-    os.chdir(inflation_master_path)
-    destroy(provider="virtualbox", tmpdir=inflation_master_tmp)
+    os.chdir(primary_nucleation_path)
+    destroy(provider="virtualbox", tmpdir=primary_nucleation_tmp)
 
 
 def inflation_ssh():
-    os.chdir(inflation_master_path)
-    ssh(provider="virtualbox", tmpdir=inflation_master_tmp)
+    os.chdir(primary_nucleation_path)
+    ssh(provider="virtualbox", tmpdir=primary_nucleation_tmp)
