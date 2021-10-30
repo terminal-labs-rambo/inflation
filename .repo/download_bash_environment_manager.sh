8b9dabf3669a12716b20b831ba49bb8989132730
Michael Verhulst <michael@terminallabs.com>
2021-10-29 19:03:33 -0500

    Delete download_bash_environment_manager.sh

--- .repo/download_bash_environment_manager.sh
+++
@@ -1,8 +0,0 @@
-apt install -y wget unzip make build-essential
-su -m vagrant <<'EOF'
-  cd /vagrant
-  mkdir -p .tmp
-  cd .tmp
-  wget -O bash-environment-manager.zip https://github.com/terminal-labs/bash-environment-manager/archive/master.zip  
-  unzip bash-environment-manager.zip
-EOF
