fe2f289c7b575bb143bdfde48d7d7aa68909c7f9
Michael Verhulst <michael@terminallabs.com>
2021-10-29 20:50:27 -0500

    started work on framework downloader system

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 import os
 import sys
 import pathlib
+import zipfile
 import configparser
+import shlex
+import json
+import subprocess
+import urllib.request
 from pathlib import Path
 from os.path import join, basename, abspath, isdir, isfile, dirname
 from setuptools import setup, find_packages
 
 assert sys.version_info >= (3, 6, 0)
+
+bash_repos = [
+    {
+    "url":"https://github.com/terminal-labs/bash-environment-shelf/archive/refs/heads/master.zip",
+    "filename":".tmp/download/bash-environment-shelf.zip"
+    }
+]
 
 with open(os.path.dirname(__file__) + "/loader.py") as f:
     code = compile(f.read(), "loader.py", "exec")
@@ -16,9 +28,19 @@ with open(os.path.dirname(__file__) + "/local.py") as f:
     code = compile(f.read(), "local.py", "exec")
     exec(code)
 
+if not os.path.exists(".tmp"):
+    os.mkdir(".tmp")
+if not os.path.exists(".tmp/download"):
+    os.mkdir(".tmp/download")
+
+urllib.request.urlretrieve(bash_repos[0]["url"], bash_repos[0]["filename"])
+with zipfile.ZipFile(".tmp/download/bash-environment-shelf.zip", "r") as zip_ref:
+    zip_ref.extractall(".tmp")
+
 _path = str(pathlib.Path(__file__).parent.absolute())
 _src = "src"
 _config = "/setup.cfg"
+
 
 config = configparser.ConfigParser()
 config.read(_path + _config)
