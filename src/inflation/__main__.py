cd72cd091060d97ce0fc3e17fa4999e2f8072b4e
Michael Verhulst <michael@terminallabs.com>
2021-11-18 11:42:35 -0600

    Update __main__.py

--- src/inflation/__main__.py
+++ src/inflation/__main__.py
@@ -1,8 +1,13 @@
 import os
+import importlib.util
 
-with open(os.path.dirname(__file__) + "/framework/lib.py") as f:
-    code = compile(f.read(), "lib.py", "exec")
-    exec(code)
+def import_mod_from_fp(module_name, filepath):
+    spec = importlib.util.spec_from_file_location(module_name, filepath)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
 
-_pgk_name = _get_pgk_name()
-main = _import_fun(f"{_pgk_name}.cli", "main")
+_fw = import_mod_from_fp('lib', os.path.dirname(__file__) + "/framework/lib.py")
+
+_pgk_name = _fw.get_pkg_name()
+main = _fw.import_fun(f"{_pgk_name}.cli", "
